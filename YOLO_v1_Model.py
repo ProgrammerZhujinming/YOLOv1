d2e7444781f1d297838989368898293083838e61
ProgrammerZhujinming <62170858+ProgrammerZhujinming@users.noreply.github.com>
2021-02-20 10:03:48 +0800

    Update YOLO_v1_Model.py

--- YOLO_v1_Model.py
+++ YOLO_v1_Model.py
@@ -88,87 +88,16 @@ class YOLO_V1(nn.Module):
         x = x.view((-1,7,7,(self.B*5 + self.Classes_Num)))
         return x
 
-
-'''
-class YOLO_V1(nn.Module):
-
-    def __init__(self,B=2,Classes_Num=20):
-        super(YOLO_V1,self).__init__()
-        self.B = B
-        self.Classes_Num = Classes_Num
-
-        self.Conv_448 = nn.Sequential(
-            nn.Conv2d(3, 64, 7, 2, 3),
-            nn.MaxPool2d(2,2),
-            nn.LeakyReLU()
-        )
-
-        self.Conv_112 = nn.Sequential(
-            nn.Conv2d(64, 192, 3, 1, 1),
-            nn.MaxPool2d(2, 2),
-            nn.LeakyReLU()
-        )
-
-        self.Conv_56 = nn.Sequential(
-            nn.Conv2d(192, 128, 1, 1, 0),
-            nn.Conv2d(128, 256, 3, 1, 1),
-            nn.Conv2d(256, 256, 1, 1, 0),
-            nn.Conv2d(256, 512, 3, 1, 1),
-            nn.MaxPool2d(2, 2),
-            nn.LeakyReLU()
-        )
-
-        self.Conv_28 = nn.Sequential(
-            nn.Conv2d(512, 256, 1, 1, 0),
-            nn.Conv2d(256, 512, 3, 1, 1),
-            nn.Conv2d(512, 256, 1, 1, 0),
-            nn.Conv2d(256, 512, 3, 1, 1),
-            nn.Conv2d(512, 256, 1, 1, 0),
-            nn.Conv2d(256, 512, 3, 1, 1),
-            nn.Conv2d(512, 256, 1, 1, 0),
-            nn.Conv2d(256, 512, 3, 1, 1),
-            nn.Conv2d(512,512,1,1,0),
-            nn.Conv2d(512,1024,3,1,1),
-            nn.MaxPool2d(2, 2),
-            nn.LeakyReLU()
-        )
-
-        self.Conv_14 = nn.Sequential(
-            nn.Conv2d(1024,512,1,1,0),
-            nn.Conv2d(512,1024,3,1,1),
-            nn.Conv2d(1024, 512, 1, 1, 0),
-            nn.Conv2d(512, 1024, 3, 1, 1),
-            nn.Conv2d(1024, 1024, 3, 1, 1),
-            nn.Conv2d(1024, 1024, 3, 2, 1),
-            nn.LeakyReLU()
-        )
-
-        self.Conv_7 = nn.Sequential(
-            nn.Conv2d(1024,1024,3,1,1),
-            nn.Conv2d(1024, 1024, 3, 1, 1),
-            nn.LeakyReLU()
-        )
-
-        self.Fc = nn.Sequential(
-            nn.Linear(7*7*1024,4096),
-            nn.Linear(4096,7 * 7 * (B*5 + Classes_Num)),
-            nn.Sigmoid()
-        )
-
-
-
-    def forward(self, x):
-        # PyTorch的卷积要求的输入是 batch_size * channels * height * width
-        x = x.permute(0,3,1,2)
-        x = self.Conv_448(x)
-        x = self.Conv_112(x)
-        x = self.Conv_56(x)
-        x = self.Conv_28(x)
-        x = self.Conv_14(x)
-        x = self.Conv_7(x)
-        x = x.view(-1,7*7*1024)
-        x = self.Fc(x)
-        x = x.view((-1,7,7,(self.B*5 + self.Classes_Num)))
-        return x
-
-'''
+        # 定义权值初始化
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                torch.nn.init.xavier_normal_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                torch.nn.init.normal_(m.weight.data, 0, 0.01)
+                m.bias.data.zero_()
