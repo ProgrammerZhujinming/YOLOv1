2b3ad8231fd1ce13f46b5010422c8ebd4d2ea0eb
ProgrammerZhujinming <62170858+ProgrammerZhujinming@users.noreply.github.com>
2021-02-21 09:44:19 +0800

    Update YOLO_V1_Train.py

--- YOLO_V1_Train.py
+++ YOLO_V1_Train.py
@@ -16,8 +16,8 @@ loss_function = Yolov1_Loss().cuda(device=1)
 
 #---------------step4:Optimizer-------------------
 import torch.optim as optim
-optimizer = optim.SGD(Yolo.parameters(),lr=5e-3,momentum=0.9,weight_decay=0.0005)
-scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,milestones=[200,400,600,20000,30000],gamma=0.9)
+optimizer = optim.SGD(Yolo.parameters(),lr=3e-3,momentum=0.9,weight_decay=0.0005)
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,milestones=[200,400,600,800,1000,2000,5000,10000],gamma=0.9)
 
 #--------------step5:Tensorboard Feature Map------------
 from tensorboardX import SummaryWriter
@@ -32,13 +32,13 @@ def feature_map_visualize(img_data, writer):
         if isinstance(m, nn.Conv2d) or isinstance(m, nn.BatchNorm2d) or \
                 isinstance(m, nn.ReLU) or isinstance(m, nn.MaxPool2d) or isinstance(m, nn.AdaptiveAvgPool2d):
             img_data = m(img_data)
-            img_data = img_data.permute(1,0,2,3)
+            img_data = img_data.permute(1, 0, 2, 3)
             img_grid = vutils.make_grid(img_data, normalize=True, scale_each=True)
             img_data = img_data.permute(1, 0, 2, 3)
             writer.add_image('feature_map', img_grid)
 
 #---------------step6:Train-------------------
-epoch = 1
+epoch = 0
 while epoch <= 2000*dataSet.Classes:
 
     loss_sum = 0
@@ -55,20 +55,18 @@ while epoch <= 2000*dataSet.Classes:
         train_data.requires_grad = True
         label_data = batch_train[1].float().cuda(device=1)
         loss = loss_function(bounding_boxes=Yolo(train_data),ground_truth=label_data)
-        loss_coord += loss[0]
-        loss_confidence += loss[1]
-        loss_classes += loss[2]
-        batch_loss = loss[0] + loss[1] + loss[2]
-        epoch_iou += loss[3]
-        epoch_object_num += loss[4]
+        batch_loss = loss[0]
+        loss_coord = loss_coord + loss[1]
+        loss_confidence = loss_confidence + loss[2]
+        loss_classes = loss_classes + loss[3]
+        epoch_iou = epoch_iou + loss[4]
+        epoch_object_num = epoch_object_num + loss[5]
         batch_loss.backward()
         optimizer.step()
-        loss_sum += batch_loss.item()
-        #writer.add_graph(loss_function, (Yolo(train_data),label_data))
-        for name, param in Yolo.named_parameters():
-            print("name:{} param:{}".format(name, param.grad))
-        print("batch_index : {} ; batch_loss : {}".format(batch_index,batch_loss.item()))
-    epoch += 1
+        batch_loss = batch_loss.item()
+        loss_sum = loss_sum + batch_loss
+        print("batch_index : {} ; batch_loss : {}".format(batch_index, batch_loss))
+    epoch = epoch + 1
     if (epoch < 1000 and epoch % 100 == 0) or epoch % 1000 == 0:
         torch.save(Yolo.state_dict(), './YOLO_V1_' + str(epoch) + '.pth')
     print("epoch : {} ; loss : {}".format(epoch,{loss_sum}))
@@ -76,10 +74,10 @@ while epoch <= 2000*dataSet.Classes:
         writer.add_histogram(name + '_grad', layer.grad.cpu().data.numpy(), epoch)
         writer.add_histogram(name + '_data', layer.cpu().data.numpy(), epoch)
     #feature_map_visualize(batch_train[0], writer)
-    writer.add_scalar('Train/Loss_sum', loss_sum, epoch + 1)
-    writer.add_scalar('Train/Loss_coord', loss_coord, epoch + 1)
-    writer.add_scalar('Train/Loss_confidenct', loss_confidence, epoch + 1)
-    writer.add_scalar('Train/Loss_classes', loss_classes, epoch + 1)
-    writer.add_scalar('Train/Epoch_iou', epoch_iou / epoch_object_num, epoch + 1)
+    writer.add_scalar('Train/Loss_sum', loss_sum, epoch)
+    writer.add_scalar('Train/Loss_coord', loss_coord, epoch)
+    writer.add_scalar('Train/Loss_confidenct', loss_confidence, epoch)
+    writer.add_scalar('Train/Loss_classes', loss_classes, epoch)
+    writer.add_scalar('Train/Epoch_iou', epoch_iou / epoch_object_num, epoch)
 
 writer.close()
